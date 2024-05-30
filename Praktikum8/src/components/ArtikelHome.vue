523c0e14619e37903fc13d7f9567c102641c85cb
LuckyFat <alvinsuherdi@students.amikom.ac.id>
2024-05-30 17:11:08 +0700

    22.11.4711

---
+++ Praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <img alt="Vue logo" class="logo" src="@/assets/unnamed.png" width="125" height="125" />
+        <h1> Ini adalah Komponen Artikel Home</h1>
+        <p>Ini adalah isi dari artikel</p>
+        <p>Ini adalah isi dari artikel</p>
+        <p>Ini adalah isi dari artikel</p>
+        <p>Ini adalah isi dari artikel</p>
+    </div>
+</template>
